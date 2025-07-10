a141ae4dbfaf712b144075aff8380d80bb3e9d09
Tumanov Artem <arti.msk2011@gmail.com>
2025-07-10 13:38:09 +0300

    Commit module tasks

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -1,8 +1,32 @@
 package com.stepup.proj;
 
+import java.util.Scanner;
+
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        for (int i = 0; i <= 5;i++ ) {
+            System.out.println("Введите первое число: ");
+            int firstNumber = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число: ");
+            int secondNumber = new Scanner(System.in).nextInt();
+
+            //сумма
+            int intResult = firstNumber + secondNumber;
+            System.out.println("Результат сложения чисел (сумма): " + intResult);
+
+            //разность
+            intResult = firstNumber - secondNumber;
+            System.out.println("Результат вычитания чисел (разность): " + intResult);
+
+            //произведение
+            intResult = firstNumber * secondNumber;
+            System.out.println("Результат умножение чисел (произведение): " + intResult);
+
+            //частное
+            double doubleResult = (double) firstNumber / secondNumber;
+            System.out.println("Результат деления чисел (частное): " + doubleResult);
+
+            System.out.println("Расчет закончен!");
+        }
     }
 }
